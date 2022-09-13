0e169c02ad7dccc5a19968e70e07a3ea611efb31
25edson25 <edsondesouzasales@gmail.com>
2022-09-13 19:57:33 -0300

    Adiciona View para Total de creditos num semestre

--- CreateViews.sql
+++ CreateViews.sql
@@ -7,6 +7,13 @@ INNER JOIN Fluxo on Fluxo_idFluxo = idFluxo
 INNER JOIN TipoFluxo on TipoFluxo_idTipoFluxo = idTipoFluxo
 INNER JOIN Curso on Fluxo.Curso_idCurso = idCurso WHERE Curso.nome LIKE 'Engenharia de Software' AND TipoFluxo.descricao LIKE 'Planejado';
 
+CREATE VIEW TotalCreditosDoSemestre AS
+SELECT Semestre_idSemestre AS Semestre, SUM(D.quantidadeCreditos) AS Creditos_Totais
+FROM Semestre_has_Disciplina AS ShD
+INNER JOIN Disciplina AS D
+ON D.idDisciplina = ShD.Disciplina_idDisciplina
+GROUP BY Semestre_idSemestre;
+
 CREATE VIEW TotalCreditosDoFluxo AS
 SELECT Fluxo_idFluxo AS ID_Fluxo, SUM(Creditos_Totais) AS Total 
 FROM Semestre AS S
