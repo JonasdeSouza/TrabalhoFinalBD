92fdbff0e91b86089edc70c42f048f200c9065ef
25edson25 <edsondesouzasales@gmail.com>
2022-09-13 19:55:45 -0300

    Adiciona View para Creditos do Fluxo

--- CreateViews.sql
+++ CreateViews.sql
@@ -6,3 +6,15 @@ INNER JOIN Disciplina on Disciplina_idDisciplina = idDisciplina
 INNER JOIN Fluxo on Fluxo_idFluxo = idFluxo
 INNER JOIN TipoFluxo on TipoFluxo_idTipoFluxo = idTipoFluxo
 INNER JOIN Curso on Fluxo.Curso_idCurso = idCurso WHERE Curso.nome LIKE 'Engenharia de Software' AND TipoFluxo.descricao LIKE 'Planejado';
+
+CREATE VIEW TotalCreditosDoFluxo AS
+SELECT Fluxo_idFluxo AS ID_Fluxo, SUM(Creditos_Totais) AS Total 
+FROM Semestre AS S
+INNER JOIN 
+	(SELECT Semestre_idSemestre AS idSemestre, SUM(D.quantidadeCreditos) AS Creditos_Totais
+	FROM Semestre_has_Disciplina AS ShD
+	INNER JOIN Disciplina AS D
+	ON D.idDisciplina = ShD.Disciplina_idDisciplina
+	GROUP BY Semestre_idSemestre) AS S_T
+ON S_T.idSemestre = S.idSemestre
+GROUP BY Fluxo_idFluxo;
