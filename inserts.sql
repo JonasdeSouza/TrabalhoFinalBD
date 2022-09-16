49d179eaf5bd9f799194f9927b3b381ca2be4db1
JonasDeSouza <jonasdesouza28@gmail.com>
2022-09-15 21:11:04 -0300

    Adicionando mais valores aos inserts

--- inserts.sql
+++ inserts.sql
@@ -186,10 +186,12 @@ INSERT INTO Pessoa (matricula, dataIngresso, email, nome, CPF, identidade, passa
 VALUES
     (NULL, "2018-03-10", "mariasouza@gmail.com", "Maria Souza", "016.103.864-31", "0.321.182", NULL, 3, 3, NULL, NULL, 4, 2, "Brasil", 3, 3, NULL),
     (180076271, "2018-03-10", "pedrosouza@gmail.com", "Pedro Souza", "046.235.012-56", "3.548.248", NULL, 1, 1, NULL, NULL, 4, 1, "Brasil", 2, 3, NULL),
-    (096023749, "1966-06-15", "victorhugo@ieee.com", "Victor Hugo", NULL, NULL, "CS265436", 2, 1, 1, NULL, 6, 3, "China", 1, 4, 1),
+    (096023749, "1996-06-15", "victorhugo@ieee.com", "Victor Hugo", NULL, NULL, "CS265436", 2, 1, 1, NULL, 6, 3, "China", 1, 4, 1),
     (170015462, "2017-02-08", "mariaeduarda@gmail.com", "Maria Eduarda", "967.177.420-20", "2.155.368", NULL, 1, 4, NULL, NULL, 4, 1, "Brasil", 1, 4, NULL),
     (010817912, "2008-09-28", "joaogomes@gmail.com", "Joao Gomes", "804.854.740-69", "0.985.716", NULL, 2, 2, 5, NULL, 3, 4, "Brasil", 1, 1, NULL),
-	(565243453, "1980-12-08", "lucas123@gmail.com", "Lucas Santos", "765.443.736-82", "2.938.560", NULL, 2, 1, 1, NULL, 6, 2, "Brasil", 2, 1, 2);
+	(565243453, "1980-12-08", "lucas123@gmail.com", "Lucas Santos", "765.443.736-82", "2.938.560", NULL, 2, 1, 1, NULL, 6, 2, "Brasil", 2, 1, 2),
+    (096023749, "2005-03-30", "mariavania@ieee.com", "Maria Vania", "020.413.470-66", "4.574.123", NULL, 2, 3, 6, NULL, 6, 3, "Brasil", 1, 2, 1),
+    (096023749, "2014-08-11", "mateuspires@unb.br", "Mateus Pires","071.975.170-52", "1.103.972", NULL, 2, 1, 7, NULL, 6, 3, "Brasil", 1, 5, 1);
 
 UPDATE Pessoa
 SET idPessoa_Filiacao = 1
